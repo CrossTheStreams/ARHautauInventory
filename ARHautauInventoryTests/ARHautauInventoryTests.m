1e91fb46305563c23bdac463c6f717d93ca54286
Andrew Hautau <arhautau@gmail.com>
2015-03-05 19:14:57 -0800

    Initial Commit

---
+++ ARHautauInventoryTests/ARHautauInventoryTests.m
@@ -0,0 +1,40 @@
+//
+//  ARHautauInventoryTests.m
+//  ARHautauInventoryTests
+//
+//  Created by Andrew Hautau on 3/5/15.
+//  Copyright (c) 2015 Andrew Hautau. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface ARHautauInventoryTests : XCTestCase
+
+@end
+
+@implementation ARHautauInventoryTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
